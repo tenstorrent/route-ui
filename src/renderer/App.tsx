30faa3f366c9ce24c9b1b2271042a3728eafde1d
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2023-06-20 12:40:31 -0400

    initial app scaffolding, electron, react, d3 etc

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,50 +1,42 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.css';
+import './App.scss';
+import { useState } from 'react';
+import FileLoader from '../main/FileLoader';
+import TenstorrentLogo from '../main/assets/TenstorrentLogo';
+import MainRouteRenderer from './MainRouteRenderer';
+import { DataSource } from '../data/DataSource';
 
-function Hello() {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
+function SplashScreen() {
+
+    return (
+        <>
+
+            <div className="splash-screen">
+
+                <FileLoader />
+
+            </div>
+        </>
+    );
 }
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
-  );
+    // eslint-disable-next-line no-use-before-define
+    const [fileData, setFileData] = useState(null);
+
+    return (
+        <>
+            <div className="Hello">
+                <TenstorrentLogo />
+            </div>
+            <DataSource.Provider value={{ fileData, setFileData }}>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<SplashScreen />} />
+                        <Route path="/render" element={<MainRouteRenderer />} />
+                    </Routes>
+                </Router>
+            </DataSource.Provider>
+        </>
+    );
 }
