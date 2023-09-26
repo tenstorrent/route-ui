f27799197ecc0724885e1a9241beca948f4051ea
Asher Minden-Webb <amindenwebb@tenstorrent.com>
2023-09-26 00:07:50 -0400

    Fix findFile filter bug

--- src/utils/Folder.ts
+++ src/utils/Folder.ts
@@ -1,4 +1,47 @@
-export default async function validateFolder(dirPath: string) {
-    console.warn('Folder validation not implemented');
+import fs, {Dirent} from 'fs';
+import path from 'path';
+
+export async function readDirEntries(dirPath: string): Promise<Dirent[]> {
+    return new Promise<Dirent[]>((resolve, reject) => {
+        fs.readdir(dirPath, {withFileTypes: true}, (err, files) => {
+            if (err) reject(err);
+            else resolve(files);
+        });
+    });
+}
+
+export async function findFile(
+    searchPath: string,
+    searchQuery: string,
+    isDir = false,
+    maxDepth = 2
+): Promise<string[]> {
+    const files = (await readDirEntries(searchPath)).filter(
+        (file) =>
+            ((isDir && !file.isDirectory()) || (!isDir && file.isFile())) &&
+            file.name === searchQuery
+    );
+    if (files.length > 0) {
+        const results = files.map((dirEntry) => path.join(searchPath, dirEntry.name));
+        return results;
+    }
+    if (maxDepth === 0) {
+        return [];
+    }
+    const subfolderResults = await Promise.all(
+        files
+            .filter((dirEntry) => dirEntry.isDirectory())
+            .map((dirEntry) => findFile(dirEntry.path, searchQuery, isDir, maxDepth - 1))
+    );
+    return subfolderResults.flat();
+}
+
+export async function validateFolder(dirPath: string) {
+    const REQUIRED_SUBFOLDERS = ['analyzer_results', 'graph_descriptors'];
+    if (!fs.existsSync(dirPath)) {
+        return false;
+    }
+    const directoryContents = await readDirEntries(dirPath);
+
     return true;
 }
