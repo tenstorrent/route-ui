afd1b08eec0d635c60efcd95bd33c53960fe2d19
Asher Minden-Webb <amindenwebb@tenstorrent.com>
2023-10-23 19:00:30 -0400

    Add queues column to properties panel (in dev environment)

--- src/renderer/PropertiesPanel.tsx
+++ src/renderer/PropertiesPanel.tsx
@@ -1,9 +1,24 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext, useMemo, useState } from 'react';
 import { Tab, TabId, Tabs } from '@blueprintjs/core';
 import ComputeNodesPropertiesTab from './components/properties-panel/ComputeNodesPropertiesTab';
 import OperationsPropertiesTab from './components/properties-panel/OperationsPropertiesTab';
 import PipesPropertiesTab from './components/properties-panel/PipesPropertiesTab';
 import DataSource, { GridContext } from '../data/DataSource';
+
+function QueuesPropertiesTab() {
+    const { chip } = useContext(DataSource);
+    const queuesList = useMemo(() => (chip ? [...chip.queues] : []), [chip]);
+
+    return (
+        <div className='queues-wrap list-wrap'>
+            <div className='scrollable-content'>
+                {queuesList.map((queue) => {
+                    return <div>{queue.name}</div>;
+                })}
+            </div>
+        </div>
+    );
+}
 
 export default function PropertiesPanel() {
     const [selectedTab, setSelectedTab] = useState<TabId>('tab1');
@@ -13,11 +28,15 @@ export default function PropertiesPanel() {
             <Tabs id='my-tabs' selectedTabId={selectedTab} onChange={setSelectedTab} className='properties-tabs'>
                 <Tab id='tab1' title='Compute Node' panel={<ComputeNodesPropertiesTab />} />
                 {/* TODO: abstract this into a global state */}
-                {chip?.pipes.size > 0 && (
+                {chip && chip.pipes.size > 0 && (
                     //
                     <Tab id='tab2' title='All pipes' panel={<PipesPropertiesTab />} />
                 )}
                 <Tab id='tab3' title='Operations' panel={<OperationsPropertiesTab />} />
+
+                {process.env.NODE_ENV === 'development' && (
+                    <Tab id='tab4' title='Queues' panel={<QueuesPropertiesTab />} />
+                )}
             </Tabs>
         </div>
     );
