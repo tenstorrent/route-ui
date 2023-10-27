49524eb06b12962bffe98e0fe3abbc16c1aa051b
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2023-10-27 15:21:59 -0400

    fix formatting issue

--- src/renderer/MainRouteRenderer.tsx
+++ src/renderer/MainRouteRenderer.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import './scss/MainRouteRenderer.scss';
 import GridRender from './GridRender';
 import PropertiesPanel from './PropertiesPanel';
 import Chip from '../data/Chip';
-import {SideBar} from './components/SideBar';
-import {getDockOpenState, RootState} from '../data/store';
+import { SideBar } from './components/SideBar';
+import { getDockOpenState, RootState } from '../data/store';
 import BottomDock from './components/BottomDock';
 
 export interface MainRouteRendererProps {
     updateData: (data: Chip) => void;
 }
 
-const MainRouteRenderer: React.FC<MainRouteRendererProps> = ({ updateData }) => {
+const MainRouteRenderer: React.FC<MainRouteRendererProps> = ({updateData}) => {
     const isDockOpen = useSelector((state: RootState) => getDockOpenState(state));
     const isDetailedViewOpen = useSelector((state: RootState) => state.detailedView.isOpen);
     return (
-        <div className={`outer-wrapper ${isDockOpen ? 'dock-open' : ''} ${isDetailedViewOpen ? 'detailed-view-open' : ''}`}>
+        <div
+            className={`outer-wrapper ${isDockOpen ? 'dock-open' : ''} ${isDetailedViewOpen ? 'detailed-view-open' : ''}`}>
             <div className='main-wrapper'>
-                <SideBar updateData={updateData} />
+                <SideBar updateData={updateData}/>
                 <div className='main-container'>
-                    <GridRender />
-                    <PropertiesPanel />
+                    <GridRender/>
+                    <PropertiesPanel/>
                 </div>
             </div>
-            <BottomDock />
+            <BottomDock/>
         </div>
     );
 };
