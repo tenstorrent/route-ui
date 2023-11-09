4dfd015b0fb1b4b9220b2abecd6d2ea7fc5ae306
Asher Minden-Webb <amindenwebb@tenstorrent.com>
2023-11-09 16:14:33 -0500

    Add Queue Location detail to GraphVertexDetail component

--- src/renderer/components/GraphVertexDetails.tsx
+++ src/renderer/components/GraphVertexDetails.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Checkbox } from '@blueprintjs/core';
 import { GraphVertex, GraphVertexType } from '../../data/GraphTypes';
 import SelectableOperation from './SelectableOperation';
-import { Checkbox } from '@blueprintjs/core';
-import { useDispatch, useSelector } from 'react-redux';
 import { RootState, selectGroup } from '../../data/store';
 
 const GraphVertexDetails = (props: { graphNode: GraphVertex }): React.ReactElement | null => {
@@ -22,8 +22,30 @@ const GraphVertexDetails = (props: { graphNode: GraphVertex }): React.ReactEleme
     if (inputs.length === 0 && outputs.length === 0) {
         return null;
     }
+
+    const parseQueueLocation = (locationString: string) => {
+        const match = locationString.match(/LOCATION::(\w+)/);
+        if (match !== null) {
+            return match[1];
+        }
+        return null;
+    };
+
     return (
-        <div className='operation-details' style={{ color: '#000', marginLeft: '20px' }}>
+        <div className='graph-vertex-details'>
+            {graphNode.vertexType === GraphVertexType.QUEUE && graphNode.details && (
+                <div className='queue-details'>
+                    <div className='queue-detail-item'>
+                        {/* TODO: Find out the string format and possible vales for Queue Location (and other details) and convert to an enum,
+                              so we're not parsing raw data
+                          */}
+                        <h5 className='queue-detail-label'>Queue Location:</h5>
+                        <div className='queue-detail-value'>
+                            {parseQueueLocation(graphNode.details.location)} (Device {graphNode.details['device-id']})
+                        </div>
+                    </div>
+                </div>
+            )}
             {inputs.length > 0 && <h5 className='io-label'>Inputs:</h5>}
             {inputs.map((operand) => (
                 <div className='operation-operand' key={operand.name}>
