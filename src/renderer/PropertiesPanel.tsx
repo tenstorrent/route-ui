c1fd007cb3e4bca93af30a7ad57c536eb51a5f36
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2023-11-24 12:33:23 -0500

    dram queue association, selectable queues fixes,

--- src/renderer/PropertiesPanel.tsx
+++ src/renderer/PropertiesPanel.tsx
@@ -1,10 +1,12 @@
 import React, { useContext, useState } from 'react';
-import { Tab, TabId, Tabs } from '@blueprintjs/core';
+import { Icon, Tab, TabId, Tabs } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
 import ComputeNodesPropertiesTab from './components/properties-panel/ComputeNodesPropertiesTab';
 import OperationsPropertiesTab from './components/properties-panel/OperationsPropertiesTab';
 import PipesPropertiesTab from './components/properties-panel/PipesPropertiesTab';
 import DataSource, { GridContext } from '../data/DataSource';
 import QueuesPropertiesTab from './components/properties-panel/QueuesPropertiesTab';
+import QueueIcon from '../main/assets/QueueIcon';
 
 export default function PropertiesPanel() {
     const [selectedTab, setSelectedTab] = useState<TabId>('tab-nodes');
@@ -12,16 +14,16 @@ export default function PropertiesPanel() {
     return (
         <div className='properties-panel'>
             <Tabs id='my-tabs' selectedTabId={selectedTab} onChange={setSelectedTab} className='properties-tabs'>
-                <Tab id='tab-nodes' title='Compute Node' panel={<ComputeNodesPropertiesTab />} />
+                <Tab id='tab-nodes' title='Nodes' panel={<ComputeNodesPropertiesTab />} />
                 {/* TODO: abstract this into a global state */}
                 {chip?.hasPipes && (
                     //
-                    <Tab id='tab-pipes' title='All pipes' panel={<PipesPropertiesTab />} />
+                    <Tab id='tab-pipes' title={<span>Pipes <Icon icon={IconNames.FILTER} /></span>} panel={<PipesPropertiesTab />} />
                 )}
-                <Tab id='tab-ops' title='Operations' panel={<OperationsPropertiesTab />} />
+                <Tab id='tab-ops' title={<span>Operations <Icon icon={IconNames.CUBE} /></span>} panel={<OperationsPropertiesTab />} />
                 {chip?.hasQueues && (
                     //
-                    <Tab id='tab-queues' title='Queues' panel={<QueuesPropertiesTab />} />
+                    <Tab id='tab-queues' title={<span>Queues <QueueIcon /> </span>} panel={<QueuesPropertiesTab />} />
                 )}
             </Tabs>
         </div>
