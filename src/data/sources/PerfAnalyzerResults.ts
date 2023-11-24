f7d1adf970f2d604c2673b3c14ac5bb30cab624c
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2023-11-24 16:48:16 -0500

    new file format support

--- src/data/sources/PerfAnalyzerResults.ts
+++ src/data/sources/PerfAnalyzerResults.ts
@@ -1,7 +1,21 @@
 import type { GraphName, OperationName } from '../GraphTypes';
 
 /** Perf Analyzer results for the given graph name, for a specific core */
-export interface CorePerfJson {
+export interface CorePerfJson extends MeasurementsJSON {}
+
+export interface PerfAnalyzerResultsJson {
+    [coreId: string]: CorePerfJson; // Maps a core ID to a perf analyzer result
+}
+
+export interface PerfAnalyzerResultsPerOpJSON {
+    [opName: string]: {
+        'op-attributes': OpAttributesJSON;
+        'op-measurements': OpMeasurementsJSON;
+        'core-measurements': CoreMeasurementsJSON;
+    };
+}
+
+export interface OpAttributesJSON {
     /** The epoch IDs that are captured in this analyzer result. Should be the same for every core result in this graph. */
     global_epoch_ids: number[];
 
@@ -27,25 +41,23 @@ export interface CorePerfJson {
      *  Format `[${width: number},${height: number}]`
      */
     grid_size: string;
+}
 
-    /** Key result for ranking op & core performance */
-    kernel_total_runtime: number;
+export interface OpMeasurementsJSON extends MeasurementsJSON {}
 
+interface MeasurementsJSON {
+    warnings: string;
+    kernel_total_runtime: number; // Key result for ranking op & core performance
     kernel_runtime_per_input: number;
     model_runtime_per_input: number;
     kernel_math_utilization: number;
     model_math_utilization: number;
-
-    /** Bandwidth-limited factor. This is a key result for analyzing total runtime. */
-    bw_limited_factor: number;
-
+    bw_limited_factor: number; // Bandwidth-limited factor. This is a key result for analyzing total runtime.
     slowest_operand: string; // Either "output" or "input-<index>"
     bw_bound_total_runtime: number;
     bw_bound_runtime_per_input: number;
     bw_bound_math_utilization: number;
-
-    // Available vs required bandwidth for all operands
-    output_pipe_bw_0: number;
+    output_pipe_bw_0: number; // Available vs required bandwidth for all operands
     required_output_pipe_bw_0: number;
     input_pipe_bw_0: number;
     required_input_bw_0: number;
@@ -53,6 +65,12 @@ export interface CorePerfJson {
     required_input_bw_1: number;
 }
 
-export interface PerfAnalyzerResultsJson {
-    [coreId: string]: CorePerfJson; // Maps a core ID to a perf analyzer result
+export interface CoreMeasurementsJSON {
+    [coreId: string]: CorePerfJson;
 }
+
+// interface TotalPerformanceData {
+//     'op-attributes': OpAttributesJSON;
+//     'op-measurements': OpMeasurementsJSON;
+//     'core-measurements': CoreMeasurementsJSON;
+// }
