083ef723ccf352818cd207cd7fc762ac5fbb838e
Leonardo Rota-Rossi <lrotarossi@tenstorrent.com>
2023-12-04 17:26:43 -0300

    feat(grid): extracts text literals into dictionary file

--- src/renderer/components/bottom-dock/OperationsTable.tsx
+++ src/renderer/components/bottom-dock/OperationsTable.tsx
@@ -2,6 +2,7 @@
 import { Table2, Column, Cell } from '@blueprintjs/table';
 import { OpPerfJSON } from 'data/sources/PerfAnalyzerResults';
 import useOperationsTable from './useOperationsTable.hooks';
+import OperationsTableDictionary from './operationsTable.dict';
 
 function OperationsTable() {
     const { operations } = useOperationsTable();
@@ -18,24 +19,33 @@ function OperationsTable() {
 
     return (
         <Table2 className='operations-table' numRows={operations.length} enableColumnHeader>
-            <Column name='Operation' cellRenderer={operationRenderer} />
-            <Column name='Grid' cellRenderer={(rowIndex) => cellRenderer('grid_size', rowIndex)} />
+            <Column name={OperationsTableDictionary.operation} cellRenderer={operationRenderer} />
             <Column
-                name='Kernel Total Runtime'
+                name={OperationsTableDictionary.grid_size}
+                cellRenderer={(rowIndex) => cellRenderer('grid_size', rowIndex)}
+            />
+            <Column
+                name={OperationsTableDictionary.kernel_total_runtime}
                 cellRenderer={(rowIndex) => cellRenderer('kernel_total_runtime', rowIndex)}
             />
             <Column
-                name='Kernel Math Utilization'
+                name={OperationsTableDictionary.kernel_math_utilization}
                 cellRenderer={(rowIndex) => cellRenderer('kernel_math_utilization', rowIndex)}
             />
-            <Column name='BW Limited Factor' cellRenderer={(rowIndex) => cellRenderer('bw_limited_factor', rowIndex)} />
-            <Column name='Slowest Operand' cellRenderer={(rowIndex) => cellRenderer('slowest_operand', rowIndex)} />
             <Column
-                name='BW Bound Total Runtime'
+                name={OperationsTableDictionary.bw_limited_factor}
+                cellRenderer={(rowIndex) => cellRenderer('bw_limited_factor', rowIndex)}
+            />
+            <Column
+                name={OperationsTableDictionary.slowest_operand}
+                cellRenderer={(rowIndex) => cellRenderer('slowest_operand', rowIndex)}
+            />
+            <Column
+                name={OperationsTableDictionary.bw_bound_total_runtime}
                 cellRenderer={(rowIndex) => cellRenderer('bw_bound_total_runtime', rowIndex)}
             />
             <Column
-                name='BW Bound Math Utilization'
+                name={OperationsTableDictionary.bw_bound_math_utilization}
                 cellRenderer={(rowIndex) => cellRenderer('bw_bound_math_utilization', rowIndex)}
             />
         </Table2>
