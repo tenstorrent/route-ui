4c57b792046cc0ad43c07b73c7c79a5ab7c19889
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2023-12-15 10:29:14 -0500

    a11y: keyboard focus ring

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -4,6 +4,7 @@ import './App.scss';
 import { useMemo, useState } from 'react';
 import store from 'data/store/createStore';
 
+import { FocusStyleManager } from '@blueprintjs/core';
 import TenstorrentLogo from '../main/assets/TenstorrentLogo';
 import DataSource, { GridContext } from '../data/DataSource';
 import Chip from '../data/Chip';
@@ -15,6 +16,7 @@ export default function App() {
     // @ts-ignore
     const [chip, setChip] = useState<Chip>(null);
     const memoizedChip = useMemo<GridContext>(() => ({ chip, setChip }), [chip]);
+    FocusStyleManager.onlyShowFocusOnTabs();
     return (
         <Provider store={store}>
             <DataSource.Provider value={memoizedChip}>
