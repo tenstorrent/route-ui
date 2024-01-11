caa2ae730b3fbf0e1e005d4702197ea8de0922d4
mcampos <mcampos@tenstorrent.com>
2024-01-11 16:38:22 -0500

    feat: improve naming and make log type optional

--- src/data/store/slices/logging.slice.ts
+++ src/data/store/slices/logging.slice.ts
@@ -2,27 +2,27 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import { LogType } from '../../Types';
 
-export interface Log<T extends LogType = LogType> {
+export interface LogEntry<T extends LogType = LogType> {
     logType: T;
     timestamp: number;
     message: string;
 }
 
 export interface LoggingState {
-    logs: Array<Log>;
+    entryList: Array<LogEntry>;
 }
 
 const loggingInitialState: LoggingState = {
-    logs: [],
+    entryList: [],
 };
 
 export const loggingSlice = createSlice({
     name: 'logging',
     initialState: loggingInitialState,
     reducers: {
-        logMessage: (state, action: PayloadAction<{ type: LogType; message: string }>) => {
-            state.logs.push({
-                logType: action.payload.type,
+        pushEntry: (state, action: PayloadAction<{ type?: LogType; message: string }>) => {
+            state.entryList.push({
+                logType: action.payload.type ?? LogType.LOG,
                 timestamp: Date.now(),
                 message: action.payload.message,
             });
@@ -30,6 +30,6 @@ export const loggingSlice = createSlice({
     },
 });
 
-export const { logMessage } = loggingSlice.actions;
+export const { pushEntry } = loggingSlice.actions;
 
 export const loggingReducer = loggingSlice.reducer;
