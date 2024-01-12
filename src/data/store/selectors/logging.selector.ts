541a0e8f4a930890421969f20a1675731a1a5834
mcampos <mcampos@tenstorrent.com>
2024-01-12 10:23:19 -0500

    feat: implement redux selector

---
+++ src/data/store/selectors/logging.selector.ts
@@ -0,0 +1,11 @@
+import { LogLevel } from '../../Types';
+import { RootState } from '../createStore';
+
+export const getLogEntry = (state: RootState, id: number) => state.logging.entryList[id];
+
+export const getLogEntries = (state: RootState) => state.logging.entryList;
+
+export const getLogEntriesByType = (state: RootState, type: LogLevel) =>
+    state.logging.entryList.filter((e) => e.logType === type);
+
+export const getOutputsToConsole = (state: RootState) => state.logging.outputsToConsole;
