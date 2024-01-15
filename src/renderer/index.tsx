62b6b60dfd978b2d59f107a7622f7e6aaa985d9d
mcampos <mcampos@tenstorrent.com>
2024-01-15 10:41:54 -0500

    feat: add handling between electron and react

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,9 +1,16 @@
 import { createRoot } from 'react-dom/client';
+
+import { updateStateOnEvent } from './utils/bridge';
+
 import App from './App';
+import { setLoggingPanelEnabledState } from '../data/store/slices/logging.slice';
+import { ElectronEvents } from '../data/Types';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(<App />);
+
+updateStateOnEvent(ElectronEvents.TOGGLE_LOGGING_PANEL, setLoggingPanelEnabledState);
 
 // calling IPC exposed from preload script
 // window.electron.ipcRenderer.once('ipc-example', (arg) => {
