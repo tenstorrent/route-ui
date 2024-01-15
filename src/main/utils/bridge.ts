7504f528697402f8e92e013acf73214f4f8182a7
mcampos <mcampos@tenstorrent.com>
2024-01-15 10:55:14 -0500

    feat: add function to listen to events from window

--- src/main/utils/bridge.ts
+++ src/main/utils/bridge.ts
@@ -1,9 +1,16 @@
-import { BrowserWindow } from 'electron';
+import { BrowserWindow, ipcMain } from 'electron';
 
 import { ElectronEvents } from '../../data/Types';
+
+export function listenToEventFromWindow<T extends Array<any>>(
+    eventName: ElectronEvents,
+    callback: (...args: T) => any,
+) {
+    ipcMain.on(eventName, (_event, ...args) => {
+        callback(...(args as T));
+    });
+}
 
 export function sendEventToWindow<T extends Array<any>>(window: BrowserWindow, eventName: ElectronEvents, ...args: T) {
     window.webContents.send(eventName, ...args);
 }
-
-export default sendEventToWindow;
