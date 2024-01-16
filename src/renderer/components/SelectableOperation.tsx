19798652b73f7b99f58d30df505bebbbf9114327
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2024-01-16 13:50:37 -0500

    op performance in table view

--- src/renderer/components/SelectableOperation.tsx
+++ src/renderer/components/SelectableOperation.tsx
@@ -1,10 +1,19 @@
 import React, { ChangeEvent, FC } from 'react';
 import { Checkbox, Icon } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
+import { useSelector } from 'react-redux';
 import HighlightedText from './HighlightedText';
 import { getGroupColor } from '../../data/ColorGenerator';
 import QueueIcon from '../../main/assets/QueueIcon';
 import { GraphVertexType } from '../../data/GraphNames';
+import { RootState } from '../../data/store/createStore';
+import {
+    getOperationPerformanceTreshold,
+    getShowOperationPerformanceGrid,
+} from '../../data/store/selectors/operationPerf.selectors';
+import { getHighContrastState } from '../../data/store/selectors/uiState.selectors';
+import { Operation } from '../../data/GraphTypes';
+import { calculateOpCongestionColor } from '../../utils/DrawingAPI';
 
 interface SelectableOperationProps {
     opName: string;
@@ -55,3 +64,33 @@ SelectableOperation.defaultProps = {
     disabled: false,
 };
 export default SelectableOperation;
+
+interface SelectableOperationPerformanceProps {
+    operation: Operation | null;
+    children: React.ReactNode;
+}
+
+export const SelectableOperationPerformance: FC<SelectableOperationPerformanceProps> = ({ operation, children }) => {
+    const render = useSelector((state: RootState) => getShowOperationPerformanceGrid(state));
+    const threshold = useSelector((state: RootState) => getOperationPerformanceTreshold(state));
+    const isHighContrast: boolean = useSelector(getHighContrastState);
+    if (!render || !operation || !operation.details) {
+        return children;
+    }
+
+    const opFactor = operation.details?.bw_limited_factor || 1;
+    if (opFactor > threshold) {
+        const congestionColor = calculateOpCongestionColor(opFactor, 0, isHighContrast);
+        return (
+            <div
+                style={{
+                    display: 'flex',
+                    color: `${congestionColor}`,
+                }}
+            >
+                {children}
+            </div>
+        );
+    }
+    return children ;
+};
