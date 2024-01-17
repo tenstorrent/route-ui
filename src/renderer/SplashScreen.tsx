ed32029e2a4b1397efb267cb720cf71f0791f60d
mcampos <mcampos@tenstorrent.com>
2024-01-17 11:26:23 -0500

    chore: remove debugging button

--- src/renderer/SplashScreen.tsx
+++ src/renderer/SplashScreen.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 import { useSelector } from 'react-redux';
 
 import { PerfDataLoader } from './components/folder-picker/FolderPicker';
-import useLogging from './hooks/useLogging.hook';
 import LogsOutput from './components/LogsOutput';
 import { getLogEntriesByType, getLogOutputEnabled } from '../data/store/selectors/logging.selector';
 import { LogLevel } from '../data/Types';
@@ -10,7 +9,6 @@ import { LogLevel } from '../data/Types';
 import './scss/SplashScreen.scss';
 
 const SplashScreen: FC = () => {
-    const logging = useLogging();
     const errorLogs = useSelector(getLogEntriesByType(LogLevel.ERROR));
     const logOutputEnabled = useSelector(getLogOutputEnabled);
 
@@ -18,9 +16,6 @@ const SplashScreen: FC = () => {
         <div className='splash-screen'>
             {/* <FileLoader /> */}
             <PerfDataLoader />
-            <button type='button' onClick={() => logging.error(`Test ${Math.random()}`)}>
-                Add log
-            </button>
             {logOutputEnabled && errorLogs.length > 0 && (
                 <div id='splash-debug-panel'>
                     <LogsOutput />
