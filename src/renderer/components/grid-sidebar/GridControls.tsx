bd828d61cdab7d776e5379a2460849e0fc732feb
mcampos <mcampos@tenstorrent.com>
2024-01-17 16:45:17 -0500

    feat: move grid controls to its own component

---
+++ src/renderer/components/grid-sidebar/GridControls.tsx
@@ -0,0 +1,61 @@
+import { FC } from 'react';
+import { Tooltip2 } from '@blueprintjs/popover2';
+import { Position, Switch } from '@blueprintjs/core';
+import { useDispatch, useSelector } from 'react-redux';
+import Collapsible from '../Collapsible';
+import {
+    getShowEmptyLinks,
+    getShowNodeLocation,
+    getShowOperationColors,
+} from '../../../data/store/selectors/linkSaturation.selectors';
+import {
+    updateShowEmptyLinks,
+    updateShowNodeLocation,
+    updateShowOperationColors,
+} from '../../../data/store/slices/linkSaturation.slice';
+
+export const GridControls: FC = () => {
+    const dispatch = useDispatch();
+
+    const showEmptyLinks = useSelector(getShowEmptyLinks);
+    const showOperationColors = useSelector(getShowOperationColors);
+    const showNodeLocation = useSelector(getShowNodeLocation);
+
+    return (
+        <Collapsible
+            contentStyles={{
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+            label='Grid Controls'
+            isOpen={false}
+        >
+            <>
+                <Tooltip2 content='Show all links overlay' position={Position.RIGHT}>
+                    <Switch
+                        checked={showEmptyLinks}
+                        label='links'
+                        onChange={(event) => dispatch(updateShowEmptyLinks(event.currentTarget.checked))}
+                    />
+                </Tooltip2>
+                <Tooltip2 content='Show all operations colors' position={Position.RIGHT}>
+                    <Switch
+                        checked={showOperationColors}
+                        label='operations'
+                        onChange={(event) => dispatch(updateShowOperationColors(event.currentTarget.checked))}
+                    />
+                </Tooltip2>
+                <Tooltip2 content='Show Compute Node locations' position={Position.RIGHT}>
+                    <Switch
+                        checked={showNodeLocation}
+                        label='location'
+                        onChange={(event) => dispatch(updateShowNodeLocation(event.currentTarget.checked))}
+                    />
+                </Tooltip2>
+                <hr />
+            </>
+        </Collapsible>
+    );
+};
+
+export default GridControls;
--- src/renderer/components/grid-sidebar/GridSidebar.tsx
+++ src/renderer/components/grid-sidebar/GridSidebar.tsx
@@ -1,29 +1,10 @@
-import { Position, Switch } from '@blueprintjs/core';
 import { FC } from 'react';
-import { Tooltip2 } from '@blueprintjs/popover2';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-    updateShowEmptyLinks,
-    updateShowNodeLocation,
-    updateShowOperationColors,
-} from 'data/store/slices/linkSaturation.slice';
-import Collapsible from '../Collapsible';
 import { CLKBandwidthControls } from './CLKBandwidthControls';
-import {
-    getShowEmptyLinks,
-    getShowNodeLocation,
-    getShowOperationColors,
-} from '../../../data/store/selectors/linkSaturation.selectors';
 import { ZoomControls } from './ZoomControls';
 import { CongestionControls } from './CongestionControls';
+import { GridControls } from './GridControls';
 
 export const GridSidebar: FC = () => {
-    const dispatch = useDispatch();
-
-    const showEmptyLinks = useSelector(getShowEmptyLinks);
-    const showOperationColors = useSelector(getShowOperationColors);
-    const showNodeLocation = useSelector(getShowNodeLocation);
-
     return (
         <div className='inner-sidebar'>
             <div className='inner-sidebar-wrap'>
@@ -42,39 +23,8 @@ export const GridSidebar: FC = () => {
                 {/*         <hr /> */}
                 {/*     </> */}
                 {/* )} */}
-                <Collapsible
-                    contentStyles={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                    }}
-                    label='Grid Controls'
-                    isOpen={false}
-                >
-                    <>
-                        <Tooltip2 content='Show all links overlay' position={Position.RIGHT}>
-                            <Switch
-                                checked={showEmptyLinks}
-                                label='links'
-                                onChange={(event) => dispatch(updateShowEmptyLinks(event.currentTarget.checked))}
-                            />
-                        </Tooltip2>
-                        <Tooltip2 content='Show all operations colors' position={Position.RIGHT}>
-                            <Switch
-                                checked={showOperationColors}
-                                label='operations'
-                                onChange={(event) => dispatch(updateShowOperationColors(event.currentTarget.checked))}
-                            />
-                        </Tooltip2>
-                        <Tooltip2 content='Show Compute Node locations' position={Position.RIGHT}>
-                            <Switch
-                                checked={showNodeLocation}
-                                label='location'
-                                onChange={(event) => dispatch(updateShowNodeLocation(event.currentTarget.checked))}
-                            />
-                        </Tooltip2>
-                        <hr />
-                    </>
-                </Collapsible>
+
+                <GridControls />
 
                 <CongestionControls />
 
