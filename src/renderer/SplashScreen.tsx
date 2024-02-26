f1d6f4358aac4114dc133dab3d1abbbfab98e3ed
mcampos <mcampos@tenstorrent.com>
2024-02-26 13:47:50 -0500

    feat: add initial loading of saved state from local storage

--- src/renderer/SplashScreen.tsx
+++ src/renderer/SplashScreen.tsx
@@ -1,17 +1,32 @@
 import { FC } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { PerfDataLoader } from './components/folder-picker/FolderPicker';
-import LogsOutput from './components/LogsOutput';
-import { getLogEntriesByType, getLogOutputEnabled } from '../data/store/selectors/logging.selector';
 import { LogLevel } from '../data/Types';
+import { getLogEntriesByType, getLogOutputEnabled } from '../data/store/selectors/logging.selector';
+import LogsOutput from './components/LogsOutput';
+import { PerfDataLoader } from './components/folder-picker/FolderPicker';
 
+import { toggleQueuesTable } from '../data/store/slices/featureFlags.slice';
+import { ElectronEvents } from '../main/ElectronEvents';
+import useAppConfig from './hooks/useAppConfig.hook';
 import './scss/SplashScreen.scss';
-import ClusterView from './components/cluster-view/ClusterView';
 
 const SplashScreen: FC = () => {
     const errorLogs = useSelector(getLogEntriesByType(LogLevel.ERROR));
     const logOutputEnabled = useSelector(getLogOutputEnabled);
+
+    const { getAppConfig } = useAppConfig();
+    const dispatch = useDispatch();
+
+    const updateFeatureFlagsStateInRedux = () => {
+        const isQueuesTableEnabled = JSON.parse(
+            getAppConfig(ElectronEvents.TOGGLE_QUEUES_TABLE) ?? '[false]',
+        )[0] as boolean;
+
+        dispatch(toggleQueuesTable(isQueuesTableEnabled));
+    };
+
+    updateFeatureFlagsStateInRedux();
 
     return (
         <div className='splash-screen'>
