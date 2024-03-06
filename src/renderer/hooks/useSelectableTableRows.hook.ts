4694f723a0a0384de0dbdb125ee6a7b2088cca59
mcampos <mcampos@tenstorrent.com>
2024-03-06 15:53:01 -0500

    fix: add missing filters to queues table select all

--- src/renderer/hooks/useSelectableTableRows.hook.ts
+++ src/renderer/hooks/useSelectableTableRows.hook.ts
@@ -1,10 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { GraphVertexType } from '../../data/GraphNames';
+import { RootState } from '../../data/store/createStore';
+import { selectOperation, selectQueue, updateNodeSelection } from '../../data/store/slices/nodeSelection.slice';
+import { getSelectedState, handleSelectAll } from '../components/bottom-dock/SharedTable';
 import type { OpTableFields } from '../components/bottom-dock/useOperationsTable.hooks';
 import type { QueuesTableFields } from '../components/bottom-dock/useQueuesTable.hook';
-import { GraphVertexType } from '../../data/GraphNames';
-import { selectOperation, selectQueue, updateNodeSelection } from '../../data/store/slices/nodeSelection.slice';
-import { RootState } from '../../data/store/createStore';
-import { getSelectedState, handleSelectAll } from '../components/bottom-dock/SharedTable';
 
 const useSelectedTableRows = () => {
     const dispatch = useDispatch();
@@ -21,6 +21,10 @@ const useSelectedTableRows = () => {
             return operationsSelectionState[name]?.selected === undefined;
         }
 
+        return queuesSelectionState[name]?.selected === undefined;
+    };
+
+    const getQueuesDisabledState = (name: string) => {
         return queuesSelectionState[name]?.selected === undefined;
     };
 
@@ -49,9 +53,12 @@ const useSelectedTableRows = () => {
             },
             (row) => !getDisabledSlowestOperand(row),
         ),
-        handleSelectAllQueues: handleSelectAll<QueuesTableFields>((row, selected) => {
-            dispatch(selectQueue({ queueName: row.name, selected }));
-        }),
+        handleSelectAllQueues: handleSelectAll<QueuesTableFields>(
+            (row, selected) => {
+                dispatch(selectQueue({ queueName: row.name, selected }));
+            },
+            (row) => !getQueuesDisabledState(row.name),
+        ),
         getCoreSelectedState: getSelectedState<OpTableFields>(
             (row) => nodesSelectionState[row.core_id]?.selected ?? false,
         ),
@@ -74,6 +81,7 @@ const useSelectedTableRows = () => {
         ),
         getQueuesSelectedState: getSelectedState<QueuesTableFields>(
             (row) => queuesSelectionState[row.name]?.selected ?? false,
+            (row) => !getQueuesDisabledState(row.name),
         ),
     };
 };
