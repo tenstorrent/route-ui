7868050c96d3aa2f4d4c1b1c325dbd1c412fb3cb
mcampos <mcampos@tenstorrent.com>
2024-03-21 15:52:08 -0400

    feat: update pipeinfodialog to render consistently and be simpler

--- src/renderer/components/PipeInfoDialog.tsx
+++ src/renderer/components/PipeInfoDialog.tsx
@@ -1,91 +1,92 @@
-import React, { FC, useContext, useEffect, useState } from 'react';
-import { Tooltip2 } from '@blueprintjs/popover2';
 import { Position } from '@blueprintjs/core';
-import { JSX } from 'react/jsx-runtime';
-import { Pipe } from '../../data/GraphOnChip';
+import { Tooltip2 } from '@blueprintjs/popover2';
+import { FC, useContext, useMemo, type PropsWithChildren } from 'react';
 import { GraphOnChipContext } from '../../data/GraphOnChipContext';
 
+import './PipeInfoDialog.scss';
+
 export interface PipeInfoDialogProps {
-    contents: React.ReactNode;
     pipeId: string;
     hide?: boolean;
+    onEnter?: () => void;
+    onLeave?: () => void;
 }
 
 /**
  * PipeInfoDialog
  * @description This wrapper component is used to display information about a Pipe Segment when the user hovers over it
  */
-const PipeInfoDialog: FC<PipeInfoDialogProps> = ({ contents, pipeId, hide }) => {
-    const [tooltipContent, setTooltipContent] = useState<JSX.Element | undefined>(undefined);
+const PipeInfoDialog: FC<PropsWithChildren<PipeInfoDialogProps>> = ({ children, pipeId, hide, onEnter, onLeave }) => {
     const graphOnChip = useContext(GraphOnChipContext).getActiveGraphOnChip();
-
-    const setupData = () => {
-        const pipe: Pipe = graphOnChip?.pipes.get(pipeId) as Pipe;
-        const output: JSX.Element[] = [];
-        if (pipe) {
-            if (pipe.producerCores.length > 0 || pipe.consumerCores.length > 0) {
-                if (pipe.producerCores.length > 0) {
-                    output.push(
-                        <div className='producer-consumer'>
-                            <h3>Producer:</h3>
-                            <h2>
-                                {[...new Set(pipe.producerCores.map((core) => graphOnChip?.getNode(core)?.operation?.name))]}
-                            </h2>
-                        </div>,
-                    );
-                }
-                if (pipe.consumerCores.length > 0) {
-                    output.push(
-                        <div className='producer-consumer'>
-                            <h3>Consumer:</h3>
-                            <h2>
-                                {[...new Set(pipe.consumerCores.map((core) => graphOnChip?.getNode(core)?.operation?.name))]}
-                            </h2>
-                        </div>,
-                    );
-                }
-            } else {
-                return undefined;
-            }
-        }else{
-            return undefined;
-        }
-        // eslint-disable-next-line react/jsx-no-useless-fragment
-        return <>{output}</>;
-    };
-
-    useEffect(() => {
-        setTooltipContent(undefined);
-    }, [graphOnChip]);
-
-    if (hide) {
-        return contents as JSX.Element;
-    }
+    const producers = useMemo(
+        () => [
+            ...new Set(
+                (graphOnChip?.pipes.get(pipeId)?.producerCores ?? []).map(
+                    (core) => graphOnChip?.getNode(core)?.operation?.name,
+                ),
+            ),
+        ],
+        [graphOnChip, pipeId],
+    );
+    const consumers = useMemo(
+        () => [
+            ...new Set(
+                (graphOnChip?.pipes.get(pipeId)?.consumerCores ?? []).map(
+                    (core) => graphOnChip?.getNode(core)?.operation?.name,
+                ),
+            ),
+        ],
+        [graphOnChip, pipeId],
+    );
 
     return (
-        <Tooltip2
-            //
-            usePortal
-            content={tooltipContent}
-            position={Position.BOTTOM_RIGHT}
-            hoverOpenDelay={150}
+        <div
+            className='pipe-info-dialog'
+            onMouseEnter={() => {
+                onEnter?.();
+            }}
+            onFocus={() => {
+                onEnter?.();
+            }}
+            onMouseOut={() => {
+                onLeave?.();
+            }}
+            onBlur={() => {
+                onLeave?.();
+            }}
         >
-            <div
-                className='pipe-info-dialog'
-                onMouseEnter={() => {
-                    if (!tooltipContent) {
-                        setTooltipContent(setupData());
-                    }
-                }}
+            <Tooltip2
+                disabled={hide || (!producers.length && !consumers.length)}
+                usePortal
+                content={
+                    <>
+                        {producers.length > 0 && (
+                            <div className='producer-consumer'>
+                                <h3>Producer:</h3>
+                                <h2>{producers}</h2>
+                            </div>
+                        )}
+                        {consumers.length > 0 && (
+                            <div className='producer-consumer'>
+                                <h3>Consumer:</h3>
+                                <h2>{consumers}</h2>
+                            </div>
+                        )}
+                    </>
+                }
+                position={Position.BOTTOM_RIGHT}
+                hoverOpenDelay={130}
             >
-                {contents}
-            </div>
-        </Tooltip2>
+                {children}
+            </Tooltip2>
+        </div>
     );
 };
 
 PipeInfoDialog.defaultProps = {
     hide: false,
+    onEnter: undefined,
+    onLeave: undefined,
 };
 
 export default PipeInfoDialog;
