4ee9612496463d172bb255cc1142140425fc980b
mcampos <mcampos@tenstorrent.com>
2024-04-09 17:06:01 -0400

    feat: update graph selector component to have function to handle graph change

--- src/renderer/components/graph-selector/GraphSelector.tsx
+++ src/renderer/components/graph-selector/GraphSelector.tsx
@@ -2,38 +2,43 @@
 //
 // SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
 
-import { FC, useContext } from 'react';
+import { FC, useContext, useEffect, useState } from 'react';
 import { GraphOnChipContext } from '../../../data/GraphOnChipContext';
-import usePerfAnalyzerFileLoader from '../../hooks/usePerfAnalyzerFileLoader.hooks';
 import PopoverMenu from '../PopoverMenu';
 
 interface GraphSelectorProps {
     disabled?: boolean;
     label?: string;
-    onSelectGraph?: (graph: string) => void;
-    autoLoadFistGraph?: boolean;
+    onSelectGraph: (graph: string) => Promise<void> | void;
+    loadFirstGraphHandler?: (graph: string) => Promise<void> | void;
 }
 
-const GraphSelector: FC<GraphSelectorProps> = ({ disabled, label, onSelectGraph, autoLoadFistGraph }) => {
+const GraphSelector: FC<GraphSelectorProps> = ({ disabled, label, onSelectGraph, loadFirstGraphHandler }) => {
     const { getActiveGraphName, graphOnChipList } = useContext(GraphOnChipContext);
-    const { loadPerfAnalyzerGraph } = usePerfAnalyzerFileLoader();
     const selectedGraph = getActiveGraphName();
     const availableGraphs = Object.keys(graphOnChipList);
+    const [isLoadingGraph, setIsLoadingGraph] = useState(false);
 
-    if (autoLoadFistGraph && !selectedGraph && availableGraphs?.length > 0) {
-        loadPerfAnalyzerGraph(availableGraphs[0]);
-    }
+    useEffect(() => {
+        (async () => {
+            if (loadFirstGraphHandler && !isLoadingGraph && !selectedGraph && availableGraphs?.length > 0) {
+                setIsLoadingGraph(true);
+                await loadFirstGraphHandler(availableGraphs[0]);
+                setIsLoadingGraph(false);
+            }
+        })();
+    }, [isLoadingGraph, selectedGraph, availableGraphs, loadFirstGraphHandler]);
 
     return (
-        <PopoverMenu // Graph picker
+        <PopoverMenu
             label={selectedGraph || (label ?? 'Select graph')}
             options={availableGraphs}
             selectedItem={selectedGraph}
             onSelectItem={async (graph) => {
-                await loadPerfAnalyzerGraph(graph);
-                onSelectGraph?.(graph);
+                await onSelectGraph(graph);
             }}
-            disabled={disabled || availableGraphs?.length === 0}
+            disabled={disabled || isLoadingGraph || availableGraphs?.length === 0}
+            loading={isLoadingGraph}
         />
     );
 };
@@ -41,8 +46,7 @@ const GraphSelector: FC<GraphSelectorProps> = ({ disabled, label, onSelectGraph,
 GraphSelector.defaultProps = {
     disabled: false,
     label: undefined,
-    onSelectGraph: undefined,
-    autoLoadFistGraph: false,
+    loadFirstGraphHandler: undefined,
 };
 
 export default GraphSelector;
