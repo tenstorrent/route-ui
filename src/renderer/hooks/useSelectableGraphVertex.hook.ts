08f46a8c63571b82bbc2582a907bd1f2608fda7f
mcampos <mcampos@tenstorrent.com>
2024-04-17 15:52:14 -0400

    make useSelectableGraphVertex methods take an optional graphName param

--- src/renderer/hooks/useSelectableGraphVertex.hook.ts
+++ src/renderer/hooks/useSelectableGraphVertex.hook.ts
@@ -5,25 +5,29 @@
 import { useContext } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { GraphOnChipContext } from '../../data/GraphOnChipContext';
-import { getSelectedOperationList, getSelectedQueueList } from '../../data/store/selectors/nodeSelection.selectors';
+import { getOperationsState, getQueuesState } from '../../data/store/selectors/nodeSelection.selectors';
 import { selectOperation, selectQueue } from '../../data/store/slices/nodeSelection.slice';
 
 const useSelectableGraphVertex = () => {
     const dispatch = useDispatch();
-    const graphName = useContext(GraphOnChipContext).getActiveGraphName();
+    const currentGraphName = useContext(GraphOnChipContext).getActiveGraphName();
 
-    const operationsSelectionState = useSelector(getSelectedOperationList(graphName));
-    const queuesSelectionState = useSelector(getSelectedQueueList(graphName));
+    const operationsState = useSelector(getOperationsState);
+    const queuesState = useSelector(getQueuesState);
 
     return {
-        disabledQueue: (name: string) => queuesSelectionState[name]?.selected === undefined,
-        disabledOperation: (name: string) => operationsSelectionState[name]?.selected === undefined,
-        selected: (name: string) =>
-            operationsSelectionState[name]?.selected ?? queuesSelectionState[name]?.selected ?? false,
-        selectQueue: (queueName: string, selected: boolean) =>
-            dispatch(selectQueue({ graphName, queueName, selected })),
-        selectOperation: (opName: string, selected: boolean) =>
-            dispatch(selectOperation({ graphName, opName, selected })),
+        disabledQueue: (name: string, graphName?: string) =>
+            queuesState[graphName ?? currentGraphName]?.[name]?.selected === undefined,
+        disabledOperation: (name: string, graphName?: string) =>
+            operationsState[graphName ?? currentGraphName]?.[name]?.selected === undefined,
+        selected: (name: string, graphName?: string) =>
+            operationsState[graphName ?? currentGraphName]?.[name]?.selected ??
+            queuesState[graphName ?? currentGraphName]?.[name]?.selected ??
+            false,
+        selectQueue: (queueName: string, selected: boolean, graphName?: string) =>
+            dispatch(selectQueue({ graphName: graphName ?? currentGraphName, queueName, selected })),
+        selectOperation: (opName: string, selected: boolean, graphName?: string) =>
+            dispatch(selectOperation({ graphName: graphName ?? currentGraphName, opName, selected })),
     };
 };
 
