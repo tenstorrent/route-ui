5dec42ae79e30f785cb016cf01e27559f186942e
mcampos <mcampos@tenstorrent.com>
2024-04-25 17:47:00 -0400

    feat: make table use bulk selector

--- src/renderer/hooks/useSelectableTableRows.hook.ts
+++ src/renderer/hooks/useSelectableTableRows.hook.ts
@@ -6,8 +6,8 @@ import { useContext } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { GraphOnChipContext } from '../../data/GraphOnChipContext';
 import { getSelectedNodeList } from '../../data/store/selectors/nodeSelection.selectors';
-import { updateNodeSelection } from '../../data/store/slices/nodeSelection.slice';
-import { getSelectedState, handleSelectAll } from '../components/bottom-dock/SharedTable';
+import { selectOperandList, updateNodeSelection } from '../../data/store/slices/nodeSelection.slice';
+import { getSelectedState } from '../components/bottom-dock/SharedTable';
 import type { OpTableFields } from '../components/bottom-dock/useOperationsTable.hooks';
 import type { QueuesTableFields } from '../components/bottom-dock/useQueuesTable.hook';
 import useSelectableGraphVertex from './useSelectableGraphVertex.hook';
@@ -15,22 +15,36 @@ import useSelectableGraphVertex from './useSelectableGraphVertex.hook';
 const useSelectedTableRows = () => {
     const dispatch = useDispatch();
     const graphName = useContext(GraphOnChipContext).getActiveGraphName();
-    const { selected, selectOperand, disabledOperand } = useSelectableGraphVertex();
+    const { selected, disabledOperand } = useSelectableGraphVertex();
 
     const nodesSelectionState = useSelector(getSelectedNodeList(graphName));
 
     return {
-        handleSelectAllCores: handleSelectAll<OpTableFields>((row, isSelected) =>
-            dispatch(updateNodeSelection({ graphName, id: row.core_id, selected: isSelected })),
-        ),
-        handleSelectAllOperands: handleSelectAll<OpTableFields | QueuesTableFields>(
-            (row, isSelected) => selectOperand(row.name, isSelected),
-            (row) => !disabledOperand(row.name),
-        ),
-        handleSelectAllSlowestOperands: handleSelectAll<OpTableFields>(
-            (row, isSelected) => selectOperand(row.slowestOperandRef?.name ?? '', isSelected),
-            (row) => !disabledOperand(row.slowestOperandRef?.name ?? ''),
-        ),
+        handleSelectAllCores: (rows: OpTableFields[], isSelected: boolean) => {
+            rows.forEach((row) => {
+                dispatch(updateNodeSelection({ graphName, id: row.core_id, selected: isSelected }));
+            });
+        },
+        handleSelectAllOperands: (rows: (OpTableFields | QueuesTableFields)[], isSelected: boolean) => {
+            const operandNames = [...new Set(rows.map((row) => row.name))];
+
+            dispatch(
+                selectOperandList({
+                    filter: (operandName) => operandNames.includes(operandName) && !disabledOperand(operandName),
+                    selected: isSelected,
+                }),
+            );
+        },
+        handleSelectAllSlowestOperands: (rows: OpTableFields[], isSelected: boolean) => {
+            const operandNames = [...new Set(rows.map((row) => row.slowestOperandRef?.name ?? ''))];
+
+            dispatch(
+                selectOperandList({
+                    filter: (operandName) => operandNames.includes(operandName) && !disabledOperand(operandName),
+                    selected: isSelected,
+                }),
+            );
+        },
         getCoreSelectedState: getSelectedState<OpTableFields>(
             (row) => nodesSelectionState[row.core_id]?.selected ?? false,
         ),
