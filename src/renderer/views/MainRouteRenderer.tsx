662234367febc4be30fc30a6ef098a75095b34c5
mcampos <mcampos@tenstorrent.com>
2024-05-09 16:07:02 -0400

    chore: fix eslint complaints

--- src/renderer/views/MainRouteRenderer.tsx
+++ src/renderer/views/MainRouteRenderer.tsx
@@ -33,31 +33,29 @@ const MainRouteRenderer: React.FC<MainRouteRendererProps> = () => {
     const { error } = usePerfAnalyzerFileLoader();
 
     return (
-        <>
-            <div
-                className={`main-route ${isDockOpen ? 'dock-open' : ''} ${isDetailedViewOpen ? 'detailed-view-open' : ''} ${!loading && (error || !graphOnChip) ? 'invalid-data' : ''} ${loading ? 'loading-data' : ''}`}
-                style={
-                    {
-                        '--js-bottom-dock-height': `${
-                            isDetailedViewOpen ? detailedViewHeight : INITIAL_DETAILS_VIEW_HEIGHT
-                        }px`,
-                    } as React.CSSProperties
-                }
-            >
-                <div className='header'>
-                    <TenstorrentLogo />
-                    <TopHeaderComponent />
-                </div>
-                <SideBar />
-                <GridSidebar />
-                <GridRender />
-                <PropertiesPanel />
-                <BottomDock isActive={isDockOpen} />
-                <div className={`main-route-loading-overlay`}>
-                    <p>Loading data...</p>
-                </div>
+        <div
+            className={`main-route ${isDockOpen ? 'dock-open' : ''} ${isDetailedViewOpen ? 'detailed-view-open' : ''} ${!loading && (error || !graphOnChip) ? 'invalid-data' : ''} ${loading ? 'loading-data' : ''}`}
+            style={
+                {
+                    '--js-bottom-dock-height': `${
+                        isDetailedViewOpen ? detailedViewHeight : INITIAL_DETAILS_VIEW_HEIGHT
+                    }px`,
+                } as React.CSSProperties
+            }
+        >
+            <div className='header'>
+                <TenstorrentLogo />
+                <TopHeaderComponent />
             </div>
-        </>
+            <SideBar />
+            <GridSidebar />
+            <GridRender />
+            <PropertiesPanel />
+            <BottomDock isActive={isDockOpen} />
+            <div className='main-route-loading-overlay'>
+                <p>Loading data...</p>
+            </div>
+        </div>
     );
 };
 
