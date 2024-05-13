aa4c4c3db6ca148a35115e075e1fab919070cf4e
mcampos <mcampos@tenstorrent.com>
2024-05-13 13:03:17 -0400

    chore: remove unused methods

--- src/data/GraphOnChipContext.tsx
+++ src/data/GraphOnChipContext.tsx
@@ -19,8 +19,6 @@ interface OperandDescriptor {
 
 interface ApplicationModelState {
     operands: Map<string, OperandDescriptor>;
-    visitedGraphsHistory: string[];
-    currentGraphIndex: number;
     graphOnChipList: {
         [graphName: string]: GraphOnChip;
     };
@@ -35,10 +33,6 @@ interface GraphOnChipContextType {
     getGraphsListByTemporalEpoch: () => Map<number, GraphRelationship[]>;
     /** @deprecated Function will be removed soon, use `getGraphOnchip` instead. */
     getActiveGraphOnChip: () => GraphOnChip | undefined;
-    getPreviousGraphName: () => string | undefined;
-    getNextGraphName: () => string | undefined;
-    selectPreviousGraph: () => void;
-    selectNextGraph: () => void;
     getGraphOnChip: (temporalEpoch: number, chipId: number) => GraphOnChip | undefined;
     getOperand: (edgeName: string) => OperandDescriptor | undefined;
     getGraphOnChipListForTemporalEpoch: (epoch: number) => { graph: GraphRelationship; graphOnChip: GraphOnChip }[];
@@ -46,8 +40,6 @@ interface GraphOnChipContextType {
 
 const applicationModelState: ApplicationModelState = {
     operands: new Map<string, OperandDescriptor>(),
-    visitedGraphsHistory: [],
-    currentGraphIndex: 0,
     graphOnChipList: {},
     graphs: new Map<string, GraphRelationship>(),
 };
@@ -59,10 +51,6 @@ const GraphOnChipContext = createContext<GraphOnChipContextType>({
     getGraphRelationshipByGraphName: () => undefined,
     getGraphsListByTemporalEpoch: () => new Map(),
     getActiveGraphOnChip: () => undefined,
-    getPreviousGraphName: () => undefined,
-    getNextGraphName: () => undefined,
-    selectPreviousGraph: () => {},
-    selectNextGraph: () => {},
     getGraphOnChip: () => undefined,
     getOperand: () => undefined,
     getGraphOnChipListForTemporalEpoch: () => [],
@@ -98,8 +86,6 @@ const GraphOnChipProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
             })
             .flat();
         setState({
-            visitedGraphsHistory: [],
-            currentGraphIndex: 0,
             graphOnChipList: Object.fromEntries(newChips.map((chip, index) => [graphs[index].name, chip])),
             graphs: new Map(graphs.map((graph) => [graph.name, graph])),
             operands: new Map(operands.map((edge) => [edge.name, edge])),
@@ -163,30 +149,6 @@ const GraphOnChipProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
         [state.graphOnChipList, state.graphs],
     );
 
-    const getPreviousGraphName = useCallback(() => {
-        return state.visitedGraphsHistory[state.currentGraphIndex - 1];
-    }, [state.currentGraphIndex, state.visitedGraphsHistory]);
-
-    const getNextGraphName = useCallback(() => {
-        return state.visitedGraphsHistory[state.currentGraphIndex + 1];
-    }, [state.currentGraphIndex, state.visitedGraphsHistory]);
-
-    const selectPreviousGraph = useCallback(() => {
-        setState((prevState) => ({
-            ...prevState,
-            currentGraphIndex: Math.max(0, prevState.currentGraphIndex - 1),
-            visitedGraphsHistory: [...prevState.visitedGraphsHistory],
-        }));
-    }, []);
-
-    const selectNextGraph = useCallback(() => {
-        setState((prevState) => ({
-            ...prevState,
-            currentGraphIndex: Math.min(prevState.visitedGraphsHistory.length - 1, prevState.currentGraphIndex + 1),
-            visitedGraphsHistory: [...prevState.visitedGraphsHistory],
-        }));
-    }, []);
-
     const getOperand = useCallback(
         (edgeName: string) => {
             return state.operands.get(edgeName);
@@ -203,10 +165,6 @@ const GraphOnChipProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
             getGraphsListByTemporalEpoch,
             getGraphOnChip,
             resetGraphOnChipState: reset,
-            getPreviousGraphName,
-            getNextGraphName,
-            selectPreviousGraph,
-            selectNextGraph,
             getOperand,
             getGraphOnChipListForTemporalEpoch,
         }),
@@ -218,10 +176,6 @@ const GraphOnChipProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
             getGraphsListByTemporalEpoch,
             getGraphOnChip,
             reset,
-            getPreviousGraphName,
-            getNextGraphName,
-            selectPreviousGraph,
-            selectNextGraph,
             getOperand,
             getGraphOnChipListForTemporalEpoch,
         ],
