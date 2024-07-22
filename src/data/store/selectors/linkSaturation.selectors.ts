730a3af07bfcc4d0597b8cf21d598bc3b6520288
mcampos <mcampos@tenstorrent.com>
2024-07-22 16:30:34 -0400

    fix: add non null assertion to satisfy typescript

--- src/data/store/selectors/linkSaturation.selectors.ts
+++ src/data/store/selectors/linkSaturation.selectors.ts
@@ -5,16 +5,16 @@
 import { RootState } from '../createStore';
 
 export const getTotalOps = (temporalEpoch: number, chipId?: number) => (state: RootState) =>
-    (chipId !== undefined
-        ? state.linkSaturation.linksPerTemporalEpoch[temporalEpoch]?.chipTotalOps?.[chipId]
-        : state.linkSaturation.linksPerTemporalEpoch[temporalEpoch]?.totalOps) || 0;
+    chipId !== undefined
+        ? state.linkSaturation.linksPerTemporalEpoch[temporalEpoch]!.chipTotalOps[chipId]!
+        : state.linkSaturation.linksPerTemporalEpoch[temporalEpoch]!.totalOps;
 export const getTotalOpsList = (temporalEpoch: number) => (state: RootState) =>
-    state.linkSaturation.linksPerTemporalEpoch[temporalEpoch].chipTotalOps;
+    state.linkSaturation.linksPerTemporalEpoch[temporalEpoch]!.chipTotalOps;
 export const getEpochNormalizedTotalOps = (temporalEpoch: number) => (state: RootState) =>
-    state.linkSaturation.linksPerTemporalEpoch[temporalEpoch].normalizedTotalOps;
+    state.linkSaturation.linksPerTemporalEpoch[temporalEpoch]!.normalizedTotalOps;
 
 export const getEpochInitialNormalizedTotalOps = (temporalEpoch: number) => (state: RootState) =>
-    state.linkSaturation.linksPerTemporalEpoch[temporalEpoch].initialNormalizedTotalOps;
+    state.linkSaturation.linksPerTemporalEpoch[temporalEpoch]!.initialNormalizedTotalOps;
 export const getLinkSaturation = (state: RootState) => state.linkSaturation.linkSaturationTreshold;
 export const getShowLinkSaturation = (state: RootState) => state.linkSaturation.showLinkSaturation;
 export const getShowNOC0 = (state: RootState) => state.linkSaturation.showNOC0;
