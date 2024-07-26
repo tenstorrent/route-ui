9e9bfb718e177ccc87cca49ce3b9a629940088c5
mcampos <mcampos@tenstorrent.com>
2024-07-26 16:24:46 -0400

    fix: resolve comments

--- src/data/store/selectors/nodeSelection.selectors.ts
+++ src/data/store/selectors/nodeSelection.selectors.ts
@@ -9,8 +9,8 @@ import type { ComputeNodeState } from '../../StateTypes';
 export const getDramHighlightState = (temporalEpoch: number, id: string) => (state: RootState) =>
     state.nodeSelection.dramNodesHighlight[temporalEpoch]?.[id];
 
-export const selectNodeSelectionById = (temporalEpoch: number, id: string) => (state: RootState) =>
-    state.nodeSelection.nodeList[temporalEpoch]?.[id];
+export const selectNodeSelectionById = (temporalEpoch: number, nodeUID: string) => (state: RootState) =>
+    state.nodeSelection.nodeList[temporalEpoch]?.[nodeUID];
 export const getOperand = (opName: string) => (state: RootState) => state.nodeSelection.operands[opName];
 
 export const getOperandState = createSelector(
@@ -28,8 +28,8 @@ export const getSelectedNodeList = (temporalEpoch: number) => (state: RootState)
 
 export const getOrderedSelectedNodeList = (temporalEpoch: number) => (state: RootState) =>
     (state.nodeSelection.selectedNodeList[temporalEpoch] ?? [])
-        .map((id) => state.nodeSelection.nodeList[temporalEpoch]?.[id])
-        .filter((node) => Boolean(node))
+        .map((nodeUID) => state.nodeSelection.nodeList[temporalEpoch]?.[nodeUID])
+        .filter((nodeState) => nodeState !== undefined)
         .toReversed() as ComputeNodeState[];
 
 export const getFocusNode = (state: RootState) => state.nodeSelection.focusNode;
