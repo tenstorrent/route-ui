267ca6cb1aebb5c72d5b47ab588b934b40490307
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2024-08-02 16:32:43 -0400

    update SPDX header

--- src/renderer/utils/linkSaturation.ts
+++ src/renderer/utils/linkSaturation.ts
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+
 import { LinkType } from 'data/Types';
 import { ETH_BANDWIDTH_INITIAL_GBS } from 'data/constants';
 
